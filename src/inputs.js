a5c8e53033b058e9aa0c358f303b54615ba42e3b
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-05-18 09:51:20 +0100

    Input parsing and tests

---
+++ src/inputs.js
@@ -0,0 +1,63 @@
+const core = require('@actions/core');
+
+/**
+ * Custom error
+ */
+class InputError extends Error {
+    constructor(...args) {
+        super(...args)
+        Error.captureStackTrace(this, InputError)
+    }
+}
+
+/**
+ * If this is an array, then return the string split
+ * by the ,
+ * Otherwise, return the value or false directly
+ *
+ * @param {string} name
+ * @param {boolean} isArray
+ * @returns {string|boolean}
+ */
+const input = (name, isArray) => {
+    const val = core.getInput(name)
+    if (isArray &&  val.length > 0) return val.split(',')
+    else if (!isArray) return val || false
+    return false
+}
+
+
+/**
+ * Generate an object of input parameters
+ * @returns {object}
+ */
+const get = () => {
+    return {
+        token: input('organisation_token', false),
+        organisation_slug: input('organisation_slug', false),
+        team_slug: input('team_slug', false),
+        artifact_names: input('artifact_names', true),
+        report_names: input('report_names', true),
+    }
+}
+
+/**
+ * Validate that {params} contains all the required
+ * input, otherwise throw an error detailing whats missing
+ * @param {object} params
+ */
+const validate = (params) => {
+    const invalidEntries = Object
+                                .entries( params )
+                                .filter(row => row.pop() === false )
+
+    if (invalidEntries.length > 0) {
+        throw new InputError(`the following input params were missing:\n - ${invalidEntries.join('\n - ')}`)
+    }
+    return true
+}
+
+module.exports = {
+    get,
+    validate
+}
---
+++ tests/inputs.test.js
@@ -0,0 +1,53 @@
+const { expect } = require('@jest/globals')
+const inputParams = require('../src/inputs')
+
+test(`Test inputParameters is all false without process env vars`, async () => {
+    const params = inputParams.get()
+    const entities = Object.entries(params)
+    const falsey = entities.filter(i => i.pop() === false)
+
+    expect(falsey.length).toEqual(entities.length)
+
+})
+
+
+test(`Test inputParameters is all false except for team slug`, async () => {
+    // set a team slug for this test
+    process.env.INPUT_TEAM_SLUG = 'test'
+    const params = inputParams.get()
+    const entities = Object.entries(params)
+    const falsey = entities.filter(i => i.pop() === false)
+
+    expect(falsey.length).toEqual(entities.length - 1)
+
+})
+
+
+test(`Test validInputParameters throws an exception without env vars`, async () => {
+    const params = inputParams.get()
+    expect.assertions(1)
+    try{
+        inputParams.validate(params)
+    } catch (e) {
+        expect(e).toBeInstanceOf(Error)
+    }
+})
+
+
+test(`Test validInputParameters validates with env vars set`, async () => {
+    // set this processes env vars
+    process.env.INPUT_ORGANISATION_TOKEN = 'test'
+    process.env.INPUT_TEAM_SLUG = 'test'
+    process.env.INPUT_ORGANISATION_SLUG = 'test'
+    process.env.INPUT_ARTIFACT_NAMES = 'test1,test2'
+    process.env.INPUT_REPORT_NAMES = 'test'
+
+    const params = inputParams.get()
+    const valid = inputParams.validate(params)
+
+    expect(valid).toBeTruthy()
+
+    expect(params.artifact_names.length).toEqual(2)
+    expect(params.report_names.length).toEqual(1)
+
+})
