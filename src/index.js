19ea5c9771cfd9dcc4fcd40306bcb1df665eaf93
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-05-25 17:22:08 +0100

    Use the directory

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const core = require('@actions/core');
-const artifact = require('@actions/artifact')
+//const artifact = require('@actions/artifact')
+const path = require('path')
 
 const inputs = require('./inputs');
 const octokit = require('./octokit')
 const download = require('./download')
 const group = require('./group')
 const load = require('./loader')
-const files = require('./files')
+const f = require('./files')
 const as = require('./as')
 
 const version = "v1.0.0"
@@ -14,28 +15,33 @@ const version = "v1.0.0"
  *
  */
 async function run() {
-  const artifactName = 'merged-report'
-  const now = Date.now()
-  const downloadDirectory = `./__downloads-${now}/`
-  const artifactDir = `./__artifact-${now}/`
   // validate the action inputs
   const params = inputs.get()
   inputs.validate(params)
   core.info(`GitHub Action inputs validated.`)
 
+  f.mkdir(`${params.directory}./__downloads`)
+  f.mkdir(`${params.directory}./__artifacts`)
+
+
+  const artifactName = 'merged-report'
+  const now = Date.now().toString()
+  const downloadDir = path.resolve(params.directory, '__downloads', now) + '/'
+  const artifactDir = path.resolve(params.directory, '__artifacts', now) + '/'
+
   // fetch the octokit using the token from the action
   const octo = octokit.get(params.token)
   const repos = await octokit.repos(octo, params.organisation_slug, params.team_slug)
   // do all the downloads
-  await download.run(octo, repos, params, downloadDirectory)
+  await download.run(octo, repos, params, downloadDir)
   // get all the report files
-  const reportFiles = await files.get(params, downloadDirectory)
+  const reportFiles = await f.get(params, downloadDir)
   // merge object to push everything into
   const loaded  = load.fromFiles(reportFiles)
   loaded.packages = group.byName(loaded.packages)
   // save the merged report as json and then markdown
-  const jsonFile = files.write(`report.${version}.json`, artifactDir, as.json(loaded) )
-  const mkFile = files.write(`report.${version}.md`, artifactDir, as.markdown(loaded) )
+  const jsonFile = f.write(`report.${version}.json`, artifactDir, as.json(loaded) )
+  const mkFile = f.write(`report.${version}.md`, artifactDir, as.markdown(loaded) )
   const files = [ `${artifactDir}report.${version}.json`, `${artifactDir}report.${version}.md` ]
 
   if (jsonFile && mkFile){
